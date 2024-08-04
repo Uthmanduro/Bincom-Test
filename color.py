f821d6fb8a579e6d1bad8529ea6a81b6eacea079
uthmanduro <udurosinlohun@gmail.com>
2024-08-04 22:15:17 +0100

    added function to add data to postgres database

--- color.py
+++ color.py
@@ -1,3 +1,10 @@
+import os 
+from dotenv import load_dotenv
+import psycopg2
+
+
+load_dotenv()
+
 # Extracted from the HTML table
 data = {
     "Monday": ["GREEN", "YELLOW", "GREEN", "BROWN", "BLUE", "PINK", "BLUE", "YELLOW", "ORANGE", "CREAM", "ORANGE", "RED", "WHITE", "BLUE", "WHITE", "BLUE", "BLUE", "BLUE", "GREEN"],
@@ -6,6 +13,17 @@ data = {
     "Thursday": ["BLUE", "BLUE", "GREEN", "WHITE", "BLUE", "BROWN", "PINK", "YELLOW", "ORANGE", "CREAM", "ORANGE", "RED", "WHITE", "BLUE", "WHITE", "BLUE", "BLUE", "BLUE", "GREEN"],
     "Friday": ["GREEN", "WHITE", "GREEN", "BROWN", "BLUE", "BLUE", "BLACK", "WHITE", "ORANGE", "RED", "RED", "RED", "WHITE", "BLUE", "WHITE", "BLUE", "BLUE", "BLUE", "WHITE"]
 }
+
+# Database connection details
+conn = psycopg2.connect(
+    # dbname=os.getenv('DB_NAME'),
+    # user=os.getenv('DB_USER'),
+    # password=os.getenv('DB_PASSWORD'),
+    # host=os.getenv('DB_HOST'),
+    # port=5433
+    os.getenv('URL')
+)
+cur = conn.cursor()
 
 from collections import Counter
 
@@ -31,6 +49,24 @@ variance = sum((count - mean_frequency) ** 2 for count in color_counts.values())
 total_colors = sum(color_counts.values())
 red_count = color_counts.get('RED', 0)
 probability_red = red_count / total_colors
+
+
+# Create table
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS colors (
+        color VARCHAR(50),
+        frequency INT
+    )
+""")
+
+# Insert data
+for color, frequency in color_counts.items():
+    cur.execute("INSERT INTO colors (color, frequency) VALUES (%s, %s)", (color, frequency))
+
+conn.commit()
+cur.close()
+conn.close()
+
 
 print("Most worn color:", most_worn_color)
 print("Median color:", median_color)
--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,16 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
+def fibonacci_sequence(n):
+    fib_sequence = [0, 1]  # Starting values for the Fibonacci sequence
+    while len(fib_sequence) < n:
+        next_value = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_value)
+    return fib_sequence
 
-fibonacci_numbers = [fibonacci(i) for i in range(50)]
-sum_fibonacci = sum(fibonacci_numbers)
-print("Sum of first 50 Fibonacci numbers:", sum_fibonacci)
+def sum_fibonacci_sequence(n):
+    fib_sequence = fibonacci_sequence(n)
+    return sum(fib_sequence)
+
+# Sum the first 50 Fibonacci numbers
+n = 50
+sum_of_fibonacci = sum_fibonacci_sequence(n)
+print(f"Sum of the first {n} Fibonacci numbers: {sum_of_fibonacci}")
+
